791be52ee0185448b948e458919ec8616fdfa53c
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2020-05-07 20:10:19 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/")
+@app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    application.run()
+    app.run(host="0.0.0.0", port=8080)
